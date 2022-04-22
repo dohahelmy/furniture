dd33467c33c9aeb07fe3f089a1a52e473be38ff8
Doha Helmy <doha.helmy@hotmail.com>
2022-04-22 08:40:17 +0200

    sliders nav and dots

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,13 @@ $(function () {
   });
 });
 
-$('.owl-carousel').owlCarousel({
+$('.common-carousel').owlCarousel({
   loop: false,
   rtl: true,
   margin: 12,
-  dots: false,
+  dots: true,
+  nav: true,
+  navText: ["<img src='assets/img/arrow.svg' alt='Next' />", "<img src='assets/img/arrow.svg' alt='Prev' />"],
   stagePadding: 25,
   responsive: {
     0: {
@@ -21,4 +23,14 @@ $('.owl-carousel').owlCarousel({
       items: 4
     },
   }
+})
+$('.banners-carousel').owlCarousel({
+  items: 1,
+  autoplay: true,
+  loop: true,
+  rtl: true,
+  center: true,
+  dots: true,
+  nav: true,
+  navText: ["<img src='assets/img/arrow.svg' alt='Next' />", "<img src='assets/img/arrow.svg' alt='Prev' />"],
 })
